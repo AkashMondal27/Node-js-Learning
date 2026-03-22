c37afbcd15d20e3f887722259a15c11805684acd
AkashMondal27 <akashmondal102003@gmail.com>
2026-03-22 11:05:10 +0530

    use readfile( )   , add notes  about filehandlling topic

--- FileHandling/file.js
+++ FileHandling/file.js
@@ -18,3 +18,26 @@ console.log(result);
     console.log(result2)
   }
 });
+
+
+fs.appendFileSync('./store.txt' , `Hey , it is a example of append \n`);
+
+
+/*  NOTE : -
+
+File handling in Node.js is the process of creating, reading, writing, 
+updating, and deleting files using the built-in fs (File System) module.
+It allows applications to store and manage data on the system.
+
+📌 Key Points:
+ 
+1. Use require('fs') to access file system
+2. Supports synchronous (blocking) and asynchronous (non-blocking) operations
+ 
+3. Common methods:
+ 
+    writeFile() → create/write file
+    readFile() → read file
+    appendFile() → add data
+    unlink() → delete file 
+ */
