a895a876b9b0e0707d701e41067888e3029ded12
AkashMondal27 <akashmondal102003@gmail.com>
2026-03-24 12:14:59 +0530

    Implement logging for incoming requests and tidy up server code

--- HTTP-Server/index.js
+++ HTTP-Server/index.js
@@ -1,10 +1,18 @@
 const http = require('http');
+const fs= require('fs');
 
 const myServer  =http.createServer((req ,res)=>{
-    console.log('new request ' );
-    res.end('hwllow buddy ');
+    // console.log('new request ' );
+     const entryLog = ` ${Date.now()} : ${req.url}New request Received\n`;
+     fs.appendFile("entryLog.txt", entryLog , (err, dat)=>{
+            res.end('hwllow buddy ');
+     })
+    
+
 });
  
 
 const PORT=8000;
-myServer.listen(PORT, ()=>{ console.log( `server started at port ${PORT}`)});
+myServer.listen(PORT, ()=>{ 
+    console.log( `server started at port ${PORT}`)
+});
