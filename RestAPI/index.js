605c32c41864d7fe22506ea4ec521e771bc73c35
AkashMondal27 <akashmondal102003@gmail.com>
2026-03-27 09:38:20 +0530

    see how make a Hybride api ,

--- RestAPI/index.js
+++ RestAPI/index.js
@@ -5,9 +5,22 @@ const PORT = 3000;
 const users = require('./MOCK_DATA.json');
 
 //1st rote to get all users
-app.get('/users',(req,res)=>{
+
+app.get('/users',(req,res)=>{   // return html page with all users
+     const html = `
+     <ul>
+     ${users.map(user => `<li>${user.first_name}</li>`).join('')}
+     </ul>
+     `
+      res.send(html);
+})
+
+//REST API route to get all users in json format
+app.get('/api/users',(req,res)=>{   
         return res.json(users);
 })
+
+
 
 
 app.listen(PORT,()=>{
