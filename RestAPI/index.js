dce15e27cffec134d41ef286442aaa90dbe61fb2
AkashMondal27 <akashmondal102003@gmail.com>
2026-03-28 12:48:28 +0530

    use maultiple request in same route

--- RestAPI/index.js
+++ RestAPI/index.js
@@ -9,7 +9,7 @@ const users = require('./MOCK_DATA.json');
 app.get('/users',(req,res)=>{   // return html page with all users
      const html = `
      <ul>
-     ${users.map(user => `<li>${user.first_name}</li>`).join('')}
+     ${users.map((user) => `<li>${user.first_name}</li>`).join('')}
      </ul>
      `
       res.send(html);
@@ -20,9 +20,27 @@ app.get('/api/users',(req,res)=>{
         return res.json(users);
 })
 
-
-
-
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`);
+app.route('/api/users/:id').get((req,res)=>{
+ const userId= Number(req.params.id);
+    const user= users.find((user) => user.id === userId) ;
+    if(user){
+        return res.json(user);
+    }else{
+        return res.status(404).json({message: 'User not found'});
+    }
 })
+.put((req,res)=>{
+    return res.json({message: 'User updated successfully'});
+})
+.delete((req,res)=>{
+    return res.json({message: 'User deleted successfully'});
+})
+.patch((req,res)=>{
+    return res.json({message: 'User updated successfully'});
+});
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
